882209c440f441de13941ad01703aed57a3a27f5
felsokning <46375514+felsokning@users.noreply.github.com>
2024-11-13 17:26:18 +0000

    Update dllmain.cpp
    Adding comments. Fixed usage string. Fixed for loop to prevent expensive copying.

--- FelsokningExt/dllmain.cpp
+++ FelsokningExt/dllmain.cpp
@@ -5,9 +5,11 @@
 #define APPNAME _T("FelsokningExt")
 #pragma warning(disable : 4996) // _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 
+//VCR003 Warnings - https://developercommunity.visualstudio.com/t/Information-message-VCR003-given-for-ext/10729403
+
 using namespace std;
 
-const char seekUsage[] = "FelsokningExt by John Bailey\n\tUsage:\n\t\t* Specify '-q' (quiet) to omit the per-thread header\n\t\t* Specify '-s' to include stacks that contain 'symbol'\n\tExample:\n\t\t!seek -s hostfxr!execute_app\n\n\tTo file an issue or feature request: https://github.com/felsokning/FelsokningExt/issues/new/choose";
+const char seekUsage[] = "FelsokningExt by John Bailey\n\tUsage:\n\t\t* Specify '-q' (quiet) to omit the per-thread header\n\t\t* Specify '-s' to include stacks that contain 'symbol'\n\tExample:\n\t\t!seek -s hostfxr!execute_app\n\n\tTo file an issue or feature request: https://github.com/felsokning/FelsokningExt/issues/new/choose\n";
 
 /// <summary>
 ///     The DebugExtensionUninitialize callback function is called by the engine to uninitialize the DbgEng extension DLL before it is unloaded.
@@ -39,11 +41,28 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 {
     switch (ul_reason_for_call)
     {
+        // The DLL is being loaded into the virtual address space of the current process as a result of the process starting up or as a result of a call to LoadLibrary. 
+        // DLLs can use this opportunity to initialize any instance data or to use the TlsAlloc function to allocate a thread local storage (TLS) index.
+        // The lpvReserved parameter indicates whether the DLL is being loaded statically or dynamically.
         case DLL_PROCESS_ATTACH:
-        case DLL_THREAD_ATTACH:
-        case DLL_THREAD_DETACH:
+
+        // The DLL is being unloaded from the virtual address space of the calling process because it was loaded unsuccessfully or the reference count has reached zero (the processes has either terminated or called FreeLibrary one time for each time it called LoadLibrary).
+        // The lpvReserved parameter indicates whether the DLL is being unloaded as a result of a FreeLibrary call, a failure to load, or process termination.
+        // The DLL can use this opportunity to call the TlsFree function to free any TLS indices allocated by using TlsAlloc and to free any thread local data.
+        // Note that the thread that receives the DLL_PROCESS_DETACH notification is not necessarily the same thread that received the DLL_PROCESS_ATTACH notification.
         case DLL_PROCESS_DETACH:
             break;
+        // The current process is creating a new thread.When this occurs, the system calls the entry - point function of all DLLs currently attached to the process.
+        // The call is made in the context of the new thread. DLLs can use this opportunity to initialize a TLS slot for the thread.
+        // A thread calling the DLL entry - point function with DLL_PROCESS_ATTACH does not call the DLL entry - point function with DLL_THREAD_ATTACH.
+        // Note that a DLL's entry-point function is called with this value only by threads created after the DLL is loaded by the process. 
+        // When a DLL is loaded using LoadLibrary, existing threads do not call the entry-point function of the newly loaded DLL.
+        case DLL_THREAD_ATTACH:
+        // A thread is exiting cleanly.
+        // If the DLL has stored a pointer to allocated memory in a TLS slot, it should use this opportunity to free the memory.
+        // The system calls the entry-point function of all currently loaded DLLs with this value.
+        // The call is made in the context of the exiting thread.
+        case DLL_THREAD_DETACH:
         default:
             break;
     }
@@ -143,7 +162,7 @@ HRESULT CALLBACK seek(_In_ PDEBUG_CLIENT8 pDebugClient, _In_opt_ PCSTR args)
                                                 ULONG symSize = 0;
                                                 ULONG64 displacement = 0;
                                                 bool shouldOutput = false;
-                                                for (DEBUG_STACK_FRAME frame : stackFrames)
+                                                for (const DEBUG_STACK_FRAME& frame : stackFrames)
                                                 {
                                                     if (SUCCEEDED(pDebugSymbols->GetNameByOffset(frame.InstructionOffset, symName, 4096, &symSize, &displacement)))
                                                     {
